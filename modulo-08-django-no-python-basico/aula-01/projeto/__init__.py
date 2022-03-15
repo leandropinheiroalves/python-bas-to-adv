6ed2b057969e0eb55acb7bedb0e5761eef386c8f
leandro <leandropinheiroalves@gmail.com>
2022-03-15 19:03:20 -0300

    Finalizada aula 01 do módulo 08

---
+++ modulo-08-django-no-python-basico/aula-01/projeto/__init__.py
@@ -0,0 +1,15 @@
+"""Aula 01: Django Olá Mundo!
+
+O que são app?
+    São como se fossem sessões do nosso site.
+    Sempre que um app for criado é necessário adicionar no INSTALLED_APPS no modulo settings.py e também adicionar
+    a url do app (sessão) no módulo urls.py
+
+Para criar um novo projeto com o django:
+django-admin startproject NOMEDOPROJETO . # o ponto no final faz o django criar apenas uma pasta com o nome do projeto
+
+Para criar um novo app:
+python manage.py startapp NOMEDOAPP
+
+
+"""
