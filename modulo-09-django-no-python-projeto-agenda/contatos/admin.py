0fe595ff71deb0afbf3dd96f9ecf4bfee23b0111
leandro <leandropinheiroalves@gmail.com>
2022-03-22 19:14:58 -0300

    Finalizada aula 04 do módulo 09, mexendo no django admin

--- modulo-09-django-no-python-projeto-agenda/contatos/admin.py
+++ modulo-09-django-no-python-projeto-agenda/contatos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    # configura a forma como os dados são exibidos na area adm
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+
+    # configura em quais das informações poderam ter links clicáveis
+    list_display_links = ('id', 'nome', 'sobrenome')
+
+    # insere um filtro dos dados
+    list_filter = ('nome', 'sobrenome')
+
+    # configura o número de dados mostrados por página
+    list_per_page = 1
+
+    # configura um campo de pesquisa nos campos informados
+    search_fields = ('nome', 'sobrenome', 'telefone')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
