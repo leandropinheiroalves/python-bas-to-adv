8f9e0eeac1c7846ee1ea1be85a63bec3763943e1
leandro <leandropinheiroalves@gmail.com>
2022-03-22 19:14:58 -0300

    Finalizada Aula 03 do módulo 09, criando a base de dados

--- modulo-09-django-no-python-projeto-agenda/contatos/models.py
+++ modulo-09-django-no-python-projeto-agenda/contatos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+"""
+CONTATOS
+id: INT (automático)
+nome: STR * (obrigatório)
+sobrenome: STR (opcional)
+telefone: STR * (obrigatório)
+email: STR (opcional)
+data_criacao: DATETIME (automático)
+descricao: texto
+categoria: CATEGORIA (outro model)
+
+ CATEGORIA
+ id: INT
+ nome: STR * (obrigatório)
+"""
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Contato(models.Model):  # nome que irá aparecer na área adimistrativa do django
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
