3c217881d204b5fbdbbae258eb104765606cfd64
leandro <leandropinheiroalves@gmail.com>
2022-03-22 22:16:52 -0300

    Finalizada aula 10 do módulo 09, ordenando e filtrando valores

--- modulo-09-django-no-python-projeto-agenda/contatos/views.py
+++ modulo-09-django-no-python-projeto-agenda/contatos/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Contato
+from django.http import Http404
 
 
 def index(request):
-    contatos = Contato.objects.all()
-    paginator = Paginator(contatos, 1)
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
+    paginator = Paginator(contatos, 5)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
@@ -16,6 +17,10 @@ def index(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato,
     })
