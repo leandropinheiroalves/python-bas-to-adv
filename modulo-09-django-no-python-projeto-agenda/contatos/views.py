ba2e0cf08fd8d0a98a31e28b32c5c250e4136939
leandro <leandropinheiroalves@gmail.com>
2022-03-23 05:08:50 -0300

    Finalizada aula 11 do módulo 09, adicionado campo busca

--- modulo-09-django-no-python-projeto-agenda/contatos/views.py
+++ modulo-09-django-no-python-projeto-agenda/contatos/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from .models import Contato
+from django.db.models import Value, Q
+from django.db.models.functions import Concat
 from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.order_by('-id').filter(mostrar=True)
-    paginator = Paginator(contatos, 5)
+    paginator = Paginator(contatos, 1)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
@@ -23,4 +26,24 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato,
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if not termo:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(nome_completo=campos).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo))
+
+    paginator = Paginator(contatos, 5)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos,
     })
